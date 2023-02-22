1925947796ddf4203fda41358dcf0c9ff0e6e70f
Arpan Mukherjee <arpanchrome@gmail.com>
2023-02-22 11:38:45 +0530

    cors error issue

--- src/chats/Gateway.ts
+++ src/chats/Gateway.ts
@@ -9,7 +9,7 @@ import { throwIfEmpty } from 'rxjs';
 import { ChatDto } from 'src/client/dtos/ChatDto.dto';
 
 
-@WebSocketGateway(4001,{cors:true})
+@WebSocketGateway(4001)
 export class  ChatGateWay  implements OnModuleInit{
   
   constructor(private chatservice:ChatsService){
